8a9dd1dda1ce571db685a89ce6135ae5517b1905
Albina Salkayeva <salkaeva.a@gmail.com>
2023-09-03 01:29:13 +0600

    Improve timestampToDate function

--- src/functions/timestampToDate.js
+++ src/functions/timestampToDate.js
@@ -1,13 +1,22 @@
 function timestampToDate(timestamp) {
   const date = timestamp.toDate();
-  const day = date.getDate();
+  let day = date.getDate();
   let month = date.getMonth() + 1;
   const year = date.getFullYear();
-  const hours = date.getHours();
-  const minutes = date.getMinutes();
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
 
   if (month < 10) {
     month = '0' + month;
+  }
+  if (day < 10) {
+    day = '0' + day;
+  }
+  if (hours < 10) {
+    hours = '0' + hours;
+  }
+  if (minutes < 10) {
+    minutes = '0' + minutes;
   }
 
   return `${year}-${month}-${day} ${hours}:${minutes}`;
