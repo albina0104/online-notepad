a5980e04095a86b851d20086bab6f093701596ae
Albina Salkayeva <salkaeva.a@gmail.com>
2023-09-10 17:54:54 +0600

    Refactor Navbar, separate into several components

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link, useNavigate } from 'react-router-dom';
 import Firestore from '../handlers/firestore';
 import { useAuthContext } from '../context/AuthContext';
 
-function Login() {
+function LoginButton() {
   const { login } = useAuthContext();
   return (
     <button type='button' className='btn btn-warning' onClick={login}>
@@ -11,7 +11,7 @@ function Login() {
   );
 }
 
-function Logout() {
+function LogoutButton() {
   const { logout } = useAuthContext();
   return (
     <button type='button' className='btn btn-danger' onClick={logout}>
@@ -20,9 +20,8 @@ function Logout() {
   );
 }
 
-function Navbar() {
+function Navigation() {
   const { currentUser } = useAuthContext();
-
   const { createNote } = Firestore;
   const navigate = useNavigate();
 
@@ -31,6 +30,89 @@ function Navbar() {
     navigate(`/note/${noteRef}`);
   };
 
+  return (
+    <ul className='navbar-nav me-auto mb-2 mb-md-0'>
+      <li className='nav-item'>
+        <Link to='/' className='nav-link active' aria-current='page'>
+          Home
+        </Link>
+      </li>
+      {currentUser && (
+        <li className='nav-item'>
+          <button type='button' onClick={newNote} className='nav-link'>
+            ➕ New note
+          </button>
+        </li>
+      )}
+    </ul>
+  );
+}
+
+function SearchForm() {
+  return (
+    <form className='d-flex' role='search'>
+      <input
+        className='form-control me-2'
+        type='search'
+        placeholder='Search'
+        aria-label='Search'
+      />
+      <button className='btn btn-outline-success' type='submit'>
+        Search
+      </button>
+    </form>
+  );
+}
+
+function Dropdown() {
+  const { currentUser } = useAuthContext();
+
+  return (
+    <ul className='navbar-nav mb-2 mb-md-0'>
+      <li className='nav-item dropdown ms-2'>
+        <button
+          className='nav-link dropdown-toggle p-md-0'
+          data-bs-toggle='dropdown'
+          aria-expanded='false'
+        >
+          {currentUser ? (
+            <img
+              src={currentUser.photoURL}
+              alt={currentUser.displayName}
+              style={{ width: '36px', height: '36px', borderRadius: '50%' }}
+            />
+          ) : (
+            'Login'
+          )}
+        </button>
+        <ul className='dropdown-menu dropdown-menu-end text-center'>
+          {currentUser && (
+            <>
+              <li>
+                <a className='dropdown-item' href='#'>
+                  {currentUser.displayName}
+                </a>
+              </li>
+              <li>
+                <hr className='dropdown-divider' />
+              </li>
+              <li>
+                <LogoutButton />
+              </li>
+            </>
+          )}
+          {!currentUser && (
+            <li>
+              <LoginButton />
+            </li>
+          )}
+        </ul>
+      </li>
+    </ul>
+  );
+}
+
+function Navbar() {
   return (
     <>
       <nav className='navbar navbar-expand-md bg-body-tertiary sticky-top'>
@@ -50,72 +132,9 @@ function Navbar() {
             <span className='navbar-toggler-icon'></span>
           </button>
           <div className='collapse navbar-collapse' id='navbarSupportedContent'>
-            <ul className='navbar-nav me-auto mb-2 mb-md-0'>
-              <li className='nav-item'>
-                <Link to='/' className='nav-link active' aria-current='page'>
-                  Home
-                </Link>
-              </li>
-              <li className='nav-item'>
-                <button type='button' onClick={newNote} className='nav-link'>
-                  ➕ New note
-                </button>
-              </li>
-            </ul>
-            <form className='d-flex' role='search'>
-              <input
-                className='form-control me-2'
-                type='search'
-                placeholder='Search'
-                aria-label='Search'
-              />
-              <button className='btn btn-outline-success' type='submit'>
-                Search
-              </button>
-            </form>
-            <li className='nav-item dropdown list-unstyled ms-2'>
-              <a
-                className='nav-link dropdown-toggle'
-                href='#'
-                role='button'
-                data-bs-toggle='dropdown'
-                aria-expanded='false'
-              >
-                {currentUser ? (
-                  <img
-                    src={currentUser.photoURL}
-                    alt={currentUser.displayName}
-                    width={'36px'}
-                    height={'36px'}
-                    style={{ borderRadius: '50%' }}
-                  />
-                ) : (
-                  'Login'
-                )}
-              </a>
-              <ul className='dropdown-menu dropdown-menu-end text-center'>
-                {currentUser && (
-                  <>
-                    <li>
-                      <a className='dropdown-item' href='#'>
-                        {currentUser.displayName}
-                      </a>
-                    </li>
-                    <li>
-                      <hr className='dropdown-divider' />
-                    </li>
-                    <li>
-                      <Logout />
-                    </li>
-                  </>
-                )}
-                {!currentUser && (
-                  <li>
-                    <Login />
-                  </li>
-                )}
-              </ul>
-            </li>
+            <Navigation />
+            <SearchForm />
+            <Dropdown />
           </div>
         </div>
       </nav>
