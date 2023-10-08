e27ea8e53a028743597ecbcabe0819784a7b0e5f
Albina Salkayeva <salkaeva.a@gmail.com>
2023-10-08 23:07:07 +0600

    Add content to Profile page

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,5 +1,33 @@
+import { useAuthContext } from '../context/AuthContext';
+
 function Profile() {
-  return <h1>Profile</h1>;
+  const { currentUser } = useAuthContext();
+
+  return (
+    <>
+      <h1 className='mt-5 mb-4 text-center'>Profile</h1>
+      <div className='d-flex justify-content-center'>
+        <div className='card mb-3' style={{ maxWidth: '540px' }}>
+          <div className='row g-0'>
+            <div className='col-md-4'>
+              <img
+                src={currentUser?.photoURL}
+                className='img-fluid rounded-start'
+                alt={currentUser?.displayName}
+                style={{ width: '200px', height: 'auto' }}
+              />
+            </div>
+            <div className='col-md-8'>
+              <div className='card-body'>
+                <h2 className='card-title'>{currentUser?.displayName}</h2>
+                <p className='card-text'>{currentUser?.email}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Profile;
