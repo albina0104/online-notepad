70337d6ac20fa604077198bd6f97a4c1d2e84a65
Albina Salkayeva <salkaeva.a@gmail.com>
2024-05-25 10:54:42 +0500

    Sort notes by timestamp of last update, most recent first

--- src/components/NoteList.jsx
+++ src/components/NoteList.jsx
@@ -16,9 +16,11 @@ function NoteList() {
       <h1 className='text-center mt-4 mb-4'>My Notes</h1>
       <div className='container mb-4'>
         <div className='row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-6 g-4'>
-          {state.visibleNotes.map((note) => (
-            <NoteCard key={note.noteId} {...note} />
-          ))}
+          {state.visibleNotes
+            .sort((a, b) => b.noteUpdatedAt - a.noteUpdatedAt)
+            .map((note) => (
+              <NoteCard key={note.noteId} {...note} />
+            ))}
         </div>
       </div>
     </>
